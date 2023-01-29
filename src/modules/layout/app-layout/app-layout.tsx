377e3ecc620e94db9d0da1f821e394126cd8578e
dinesh36 <ddinesh.36@gmail.com>
2023-01-29 18:59:05 +0530

    With this commit app will have the generic image for the google ads

--- src/modules/layout/app-layout/app-layout.tsx
+++ src/modules/layout/app-layout/app-layout.tsx
@@ -16,10 +16,10 @@ function AppLayout({ children }: {children: React.ReactNode}) {
   const renderRightContent = () => (
     <div className={styles.rightContent}>
       <div className={styles.adContent1}>
-        <img src="images/home/ad/pepsi.gif" alt="ad" />
+        <img src="images/ad/google-ads.png" alt="ad" />
       </div>
       <div className={styles.adContent2}>
-        <img src="images/home/ad/tesla.gif" alt="ad" />
+        <img src="images/ad/google-ads.png" alt="ad" />
       </div>
     </div>
   );
