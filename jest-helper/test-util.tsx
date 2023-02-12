5b592238d1a0a5217e1290714caba551479dcd30
dinesh36 <ddinesh.36@gmail.com>
2023-02-12 19:48:36 +0530

    With this commit add support for the result click event and empty result snapshot

--- jest-helper/test-util.tsx
+++ jest-helper/test-util.tsx
@@ -1,11 +1,21 @@
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import React from 'react';
+import { render } from '@testing-library/react';
 import { getStore } from '../src/store/store/store';
 
 export const getComponentWithReduxWrapper = (cmp: React.ReactNode) => {
   const store = getStore();
   return renderer.create(
+    <Provider store={store}>
+      {cmp}
+    </Provider>,
+  );
+};
+
+export const getComponentWithReduxWrapperForReactTestingLib = (cmp: React.ReactNode) => {
+  const store = getStore();
+  return render(
     <Provider store={store}>
       {cmp}
     </Provider>,
--- src/modules/result/result/__test__/result.test.tsx
+++ src/modules/result/result/__test__/result.test.tsx
@@ -1,14 +1,34 @@
-import { getComponentWithReduxWrapper } from '@jestHelper/test-util';
+import { getComponentWithReduxWrapper, getComponentWithReduxWrapperForReactTestingLib } from '@jestHelper/test-util';
+import { fireEvent } from '@testing-library/react';
 import Result from '../result';
 
-const mockSelector = jest.fn().mockReturnValue({ state: { testState: { result: '' } } });
+const mockSelector = jest.fn();
+const mockDispatch = jest.fn();
 
 jest.mock('react-redux', () => ({
   ...jest.requireActual('react-redux'),
-  useSelector: () => mockSelector,
+  useSelector: () => mockSelector(),
+  useDispatch: () => mockDispatch,
 }));
 
 describe('Result Component', () => {
+  beforeEach(() => {
+    mockSelector.mockReturnValue('');
+  });
+
+  it('Should render the blank component when the result is not there', () => {
+    const component = getComponentWithReduxWrapper(<Result />);
+    const cmp = component.toJSON();
+
+    expect(cmp).toMatchSnapshot();
+  });
+});
+
+describe('Result Component', () => {
+  beforeEach(() => {
+    mockSelector.mockReturnValue('introvert');
+  });
+
   it('Should render the blank component when the result is not there', () => {
     const component = getComponentWithReduxWrapper(<Result />);
     const cmp = component.toJSON();
@@ -17,10 +37,10 @@ describe('Result Component', () => {
   });
 
   it('should show the introvert result', () => {
-    mockSelector.mockReturnValue({ state: { testState: { result: 'introvert' } } });
-    const component = getComponentWithReduxWrapper(<Result />);
-    const cmp = component.toJSON();
-
-    expect(cmp).toMatchSnapshot();
+    const cmp = getComponentWithReduxWrapperForReactTestingLib(<Result />);
+    const buttonSelector = new RegExp('Retake Test', 'i');
+    expect(cmp.queryByText(buttonSelector)).toBeTruthy();
+    fireEvent.click(cmp.getByText(buttonSelector));
+    expect(mockDispatch).toBeCalled();
   });
 });
